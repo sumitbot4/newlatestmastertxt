ee4231f030bc4973fe36252cee5af8f3cff116eb
RajBhaiOG <abhadani0987@gmail.com>
2025-06-16 17:44:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = ""
-    DB_NAME = ""
+    BOT_TOKEN = "7167580859:AAGLqchS0nxQu2TjeZiG_u-sZhey4KuOl7Q"
+    DB_NAME = "OPHACK"
     API_ID = "28137469"
     API_HASH = "3b33568aa7119719d1f37cb15b3ae587"
-    ADMIN_ID = 
-    DB_URL = ""
-    LOG_CHANNEL =  # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    ADMIN_ID = [6568611832, 5487643307, 7510415010]
+    DB_URL = "mongodb+srv://namanjain123eudhc:opmaster@cluster0.5iokvxo.mongodb.net/?retryWrites=true&w=majority"
+    LOG_CHANNEL = -1002173308278 # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = ""
-    TUTORIAL_VIDEO = ""
+    TUTORIAL_VIDEO = "https://t.me/TheRajBhaiOG"
