14c1f258e8048897a09a1d5d1e13a5c63d709712
RajBhaiOG <abhadani0987@gmail.com>
2025-06-16 17:38:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 class Config(object):
     BOT_TOKEN = ""
     DB_NAME = ""
-    API_ID = 
-    API_HASH = ""
+    API_ID = "28137469"
+    API_HASH = "3b33568aa7119719d1f37cb15b3ae587"
     ADMIN_ID = 
     DB_URL = ""
     LOG_CHANNEL =  # Your Log Channel ID (Bot ko ADMIN BNAYE)
