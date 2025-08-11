bd479ccff6e33bac38bb134edbeb0c209b9e7039
gajendrajangid83 <saraswatisharmapbc@gmail.com>
2025-08-11 12:14:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import os
 
 class Config(object):
     BOT_TOKEN = "8088050612:AAHmlNst28C7_AIuZZCI04qFAOgEBNTegiU"
-    DB_NAME = "Saraswati sharma"
+    DB_NAME = "takkishor9784"
     API_ID = "21157244"
     API_HASH = "4981c2699bd91c7db836ec8f77e5b0f0"
     ADMIN_ID = [1783306092]
-    DB_URL = "mongodb+srv://saraswatisharmapbc:L8aRiWhBbhi8mFwN@cluster0.gxhxgye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://takkishor9784:gG73juoh44MnvJEZ@cluster0.q8hxdk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     LOG_CHANNEL = -1002758652479 # Your Log Channel ID (Bot ko ADMIN BNAYE)
-    USERLINK = ""
-    TUTORIAL_VIDEO = "https://t.me/TheRajBhaiOG"
+    USERLINK = "https://t.me/bosch12345o"
+    TUTORIAL_VIDEO = "https://t.me/bosch12345o"
 
