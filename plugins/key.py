10a182f1669fc84730264a19df646cd9f9270750
gajendrajangid83 <saraswatisharmapbc@gmail.com>
2025-08-11 12:31:08 +0530

    Create key.py

---
+++ plugins/key.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+DB_NAME = os.getenv("DB_NAME")
+DB_URL = os.getenv("DB_URL")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
